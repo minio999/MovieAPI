81ad45fcedbfa21fb06976b357c7a5ec3e8561a2
minio999 <minkowskidominik69@gmail.com>
2021-12-13 20:34:53 +0100

    init

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, render_template, redirect
+import pymongo
+from bson.objectid import ObjectId
+
+
+app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(host="localhost", port=27017)
+    mongo.server_info()
+    db = mongo.Movie
+except: 
+    print("ERROR - Cannot connect to db")
+
+
+@app.route("/", methods=["POST", "GET"])
+def mainpage():
+    if request.method == "POST":
+        try:
+            movie_name = {"name": request.form['movie_content']}
+            db.movies.insert_one(movie_name)
+            return redirect('/')
+        except Exception as ex:
+            print(ex)
+            return 'There was an issue'
+    else:
+        movies = list(db.movies.find())
+        return render_template('index.html', movies=movies)
+
+@app.route("/delete/<id>")
+def delete(id):
+    try:
+        db.movies.delete_one({'_id':ObjectId(id)})
+        return redirect('/')
+    except:
+        return 'there was a problem with deleting'
+
+@app.route('/update/<id>', methods=['GET', 'POST'])
+def update(id):
+    movie_id = db.movies.find_one({"_id": ObjectId(id)})
+    if request.method == "POST":
+        try:
+            db.movies.update_one({"_id": ObjectId(id)}, {"$set":{"name": request.form["movie_content"]}})
+            print(":DDDDDDD")
+            return redirect('/')
+        except Exception as ex:
+            print(ex)
+            return "there's issue updating task"
+    else:
+        return render_template('update.html', movie=movie_id)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
