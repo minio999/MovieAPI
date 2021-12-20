f8203c32b21d4ea9867a9838231cbee86b3f58f9
minio999 <minkowskidominik69@gmail.com>
2021-12-20 22:50:24 +0100

    refactored POST method

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, Response
 import pymongo
 from bson.objectid import ObjectId
 from flask_swagger_ui import get_swaggerui_blueprint
+import json
 
 app = Flask(__name__)
 
+
+# connecting to mongo client
 try:
-    mongo = pymongo.MongoClient(host="localhost", port=27017)
-    mongo.server_info()
+    mongo = pymongo.MongoClient(host="localhost", port=27017, serverSelectionTimeoutMS=1000)
+    mongo.server_info() # trigger exception if not connected to db
     db = mongo.Movie
 except: 
     print("ERROR - Cannot connect to db")
 
-
+# setting up swagger
 SWAGGER_URL = '/swagger'
 API_URL = '/static/json/swagger.json'
 SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
@@ -25,19 +28,18 @@ SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
 
 app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix = SWAGGER_URL)
 
-@app.route("/", methods=["POST", "GET"])
-def mainpage():
-    if request.method == "POST":
-        try:
-            movie_name = {"name": request.form['movie_content']}
-            db.movies.insert_one(movie_name)
-            return redirect('/')
-        except Exception as ex:
-            print(ex)
-            return 'There was an issue'
-    else:
-        movies = list(db.movies.find())
-        return render_template('index.html', movies=movies)
+@app.route("/movies", methods=["POST"])
+def addMovies():
+    try:
+        movie = {"name":request.form["name"], "yearMade":request.form["yearMade"], "director": request.form["director"]}
+        dbResponse = db.movies.insert_one(movie)
+        return Response(
+            response=json.dumps({"message":"user creaeted", "id":f"{dbResponse.inserted_id}"}),
+            status=200,
+            mimetype="application/json"
+        )
+    except Exception as ex:
+        print(ex) 
 
 @app.route("/delete/<id>")
 def delete(id):
@@ -53,7 +55,6 @@ def update(id):
     if request.method == "POST":
         try:
             db.movies.update_one({"_id": ObjectId(id)}, {"$set":{"name": request.form["movie_content"]}})
-            print(":DDDDDDD")
             return redirect('/')
         except Exception as ex:
             print(ex)
@@ -61,5 +62,7 @@ def update(id):
     else:
         return render_template('update.html', movie=movie_id)
 
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='127.0.0.1', port=80, debug=True)
