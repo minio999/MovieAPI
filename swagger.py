731fec3083e316324bc02db858e890db89ebae38
minio999 <minkowskidominik69@gmail.com>
2021-12-27 13:28:48 +0100

    reverting changes

--- swagger.py
+++
@@ -1,9 +0,0 @@
-from flask_swagger_ui import get_swaggerui_blueprint
-
-
-class swagger:
-    # setting app swagger setting on init and returning blueprint
-    def __init__(self):
-        SWAGGER_URL = '/swagger'
-        API_URL = '/static/json/swagger.json'
-        SWAGGER_BLUEPRINT = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config = {'app_name' : 'Movie Api'})
