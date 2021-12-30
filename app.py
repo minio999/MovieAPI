185849af6027a771a60196c5ae27cf5c3b5a4281
minio999 <minkowskidominik69@gmail.com>
2021-12-30 10:21:56 +0100

    moved modules to movie_mod.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-"""
-Main File
-"""
-import json
-from flask import Flask, request, Response
+from flask import Flask
 import pymongo
 from flask_swagger_ui import get_swaggerui_blueprint
-
+import app.movie_mod 
 
 
 app = Flask(__name__)
@@ -37,44 +33,6 @@ def hello_world():
     """ this function is made for debug purpose """
     return "<p>Hello, World!</p>"
 
-# making route for POST
-@app.route("/movies", methods=["POST"])
-def add_movies():
-    """ if route is movies and method POST add movie to database from a json file """
-    try:
-        movie = request.get_json()
-        movie['id'] = movie['name'] + movie['yearMade'] + movie['director'] # creating id column to get id by name tear made and diractor for example "terminator1984spielberg" if name is terminator, yearMade is 1984 etc
-        db_response = db.movies.insert_one(movie)
-        return Response(response=json.dumps({"message":"movie creaeted", "id":f"{db_response.inserted_id}"}), status=200, mimetype="application/json")
-    except Exception as ex:
-        print(ex)
-        return Response(response=json.dumps({"message":"cannot create movie"}), status=500, mimetype="application/json")
-
-# making route for GET
-@app.route("/movies", methods=["GET"])
-def get_movies():
-    """ on route /movies with method get, get all movies from database """
-    try:
-        data = list(db.movies.find())
-        for movie in data:
-            movie["_id"] = str(movie["_id"]) # changing ObjectID into string
-        return Response(response=json.dumps(data), status=200, mimetype="application/json")
-    except Exception as ex:
-        print(ex)
-        return Response(response=json.dumps({"message":"cannot read movies"}), status=500, mimetype="application/json")
-
-# making route for DELETE
-@app.route("/movies/<id>", methods=["DELETE"])
-def delete_movie(id):
-    """ on route /movies/<id> with <id> as a parameter and method DELETE, delete movie with <id> from database """
-    try:
-        db_response = db.movies.delete_one({"id":id})
-        if db_response.deleted_count == 1: # checking if movie was deleted
-            return Response(response=json.dumps({"message":"movie deleted", "id":f"{id}"}), status=200, mimetype="application/json")
-        return Response(response=json.dumps({"message":"movie not existing", "id":f"{id}"}), status=200, mimetype="application/json")
-    except Exception as ex:
-        print(ex)
-        return Response(response=json.dumps({"message":"cannot delete movie"}), status=500, mimetype="application/json")
 #running app
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ app/movie_mod.py
@@ -0,0 +1,41 @@
+from app import app, db
+import json
+from flask import request, Response
+
+
+@app.route("/movies", methods=["POST"])
+def add_movies():
+    """ if route is movies and method POST add movie to database from a json file """
+    try:
+        movie = request.get_json()
+        # creating id column to get id by name tear made and diractor for example "terminator1984spielberg" if name is terminator, yearMade is 1984 etc
+        movie['id'] = movie['name'] + movie['yearMade'] + movie['director']
+        db_response = db.movies.insert_one(movie)
+        return Response(response=json.dumps({"message":"movie creaeted", "id":f"{db_response.inserted_id}"}), status=200, mimetype="application/json")
+    except Exception as ex:
+        print(ex)
+        return Response(response=json.dumps({"message":"cannot create movie"}), status=500, mimetype="application/json")
+
+@app.route("/movies", methods=["GET"])
+def get_movies():
+    """ on route /movies with method get, get all movies from database """
+    try:
+        data = list(db.movies.find())
+        for movie in data:
+            movie["_id"] = str(movie["_id"]) # changing ObjectID into string
+        return Response(response=json.dumps(data), status=200, mimetype="application/json")
+    except Exception as ex:
+        print(ex)
+        return Response(response=json.dumps({"message":"cannot read movies"}), status=500, mimetype="application/json")
+
+@app.route("/movies/<id>", methods=["DELETE"])
+def delete_movie(id):
+    """ on route /movies/<id> with <id> as a parameter and method DELETE, delete movie with <id> from database """
+    try:
+        db_response = db.movies.delete_one({"id":id})
+        if db_response.deleted_count == 1: # checking if movie was deleted
+            return Response(response=json.dumps({"message":"movie deleted", "id":f"{id}"}), status=200, mimetype="application/json")
+        return Response(response=json.dumps({"message":"movie not existing", "id":f"{id}"}), status=200, mimetype="application/json")
+    except Exception as ex:
+        print(ex)
+        return Response(response=json.dumps({"message":"cannot delete movie"}), status=500, mimetype="application/json")
