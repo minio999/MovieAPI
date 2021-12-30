68004f1507bf9371f42cb40a9b7e4ee3c6ebbd94
minio999 <minkowskidominik69@gmail.com>
2021-12-30 12:45:18 +0100

    update movie_mod.py

--- movie_mod.py
+++ movie_mod.py
@@ -1,6 +1,15 @@
-from app import app, db
+from app import app
+import pymongo
 import json
 from flask import request, Response
+
+
+try:
+    mongo = pymongo.MongoClient(host='test_mongodb', port=27017, username='root', password='pass')
+    mongo.server_info() # trigger exception if not connected to db
+    db = mongo.Movie
+except:
+    print("ERROR - Cannot connect to db")
 
 
 @app.route("/movies", methods=["POST"])
